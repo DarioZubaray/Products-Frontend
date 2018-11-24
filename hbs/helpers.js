8884e6985d486d19a4a90ecd3d7588db134b88d7
dario zubaray <dzubaray@gmail.com>
2018-11-24 15:07:52 -0300

    fix varios visuales

--- hbs/helpers.js
+++ hbs/helpers.js
@@ -3,3 +3,7 @@ const hbs = require('hbs');
 hbs.registerHelper('getFullYear', () => {
   return new Date().getFullYear();
 });
+
+hbs.registerHelper('json',function(obj) {
+   return new hbs.SafeString(JSON.stringify(obj))
+});
--- public/assets/js/pagination.js
+++ public/assets/js/pagination.js
@@ -1,15 +1,44 @@
-$("#paginationFirst").click(function(){
+let page = 0;
+const url = {
+  'heroku': 'https://dariozubaray-products-backend.herokuapp.com',
+  'findAll': '/api/product/findAll',
+}
+
+$("#paginationFirst").click(() => {
   console.log("Go to first page");
+  page = 0;
+  getPage(page);
 });
 
-$("#paginationPrevious").click(()=>{
+$("#paginationPrevious").click(() => {
   console.log("Go to previous page");
+  if(page > 1) {
+    page -= 1;
+  }
+  getPage(page);
 });
 
-$("#paginationNext").click(function(){
+$("#paginationNext").click(() => {
   console.log("Go to next page");
+  page += 1;
+  getPage(page);
 });
 
-$("#paginationLast").click(function(){
+$("#paginationLast").click(() => {
   console.log("Go to last page");
+  page = _products.totalPages;
+  getPage(page);
 });
+
+function getPage(page){
+  $.ajax({
+    url: `${url.heroku}${url.findAll}?page=${page}`,
+    type: 'get',
+    success: function (data) {
+        console.info(data);
+        $('#productsTable').html(data.content);
+      }
+    }).fail(function() {
+      alert( "error" );
+  });
+}
